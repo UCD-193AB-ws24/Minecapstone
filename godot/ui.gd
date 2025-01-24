234b8398a41c1bde32bbb667bd549c3e89a74937
lemontine <computereality@gmail.com>
2025-01-24 00:06:34 -0800

    implemented and optimized runtime navmesh baking, adjusted navigationAgent parameters to ideal state

--- godot/ui.gd
+++ godot/ui.gd
@@ -4,8 +4,7 @@ extends Control
 
 func _physics_process(_delta: float) -> void:
 	label.text = "FPS: " + str(Engine.get_frames_per_second())
-	
-	var chunk_manager = get_node("/root/World/ChunkManager")
+	var chunk_manager = get_node("/root/World/NavigationMesher/ChunkManager")
 	if chunk_manager:
 		var player_chunk_position = chunk_manager.GetPlayerChunkPosition()
 		label.text += "\nPlayer Chunk Position: " + str(player_chunk_position)
