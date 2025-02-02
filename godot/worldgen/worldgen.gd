1e7fef97fff8b7d0d17ed7fa469ed9fda67b0bb7
lemontine <computereality@gmail.com>
2025-02-02 00:12:34 -0800

    Code cleaning, debugging options in worldgen code, to increase usability

--- godot/worldgen/worldgen.gd
+++ godot/worldgen/worldgen.gd
@@ -32,8 +32,6 @@ const BIOME_COLORS = [
 
 var biome_indices = []
 var generation_thread: Thread
-var _threaded_biome_texture: ImageTexture
-var _threaded_normal_map_texture: ImageTexture
 
 func _ready():
 	# Load and process TP_map into biome indices
@@ -69,31 +67,38 @@ func _threaded_generate():
 	# Compute average temperature and precipitation per Voronoi cell
 	var temperature_averages = average_voronoi(temperature_noise)
 	var precipitation_averages = average_voronoi(precipitation_noise)
-	var land_ocean_mask = create_land_ocean_mask(height_noise)			# Compute mask, averages, and final biome map image
-	
+	# Compute mask, averages, and final biome map image
+	var land_ocean_mask = create_land_ocean_mask(height_noise)
+	# Combine the smoothened and detailed height maps between land and ocean
+	var combined_height_image = create_combined_height_image(land_ocean_mask)
+
+	# Debug voronoi noise
+	display_voronoi(Vector3(-7, 3.5, 0))
+
+	# Debug temperature to voronoi average
 	display_noise(temperature_noise, Vector3(0, 7, 0))
 	display_dict(temperature_averages, Vector3(0, 0, 0))
+	
+	# Debug precipitation to voronoi average
 	display_noise(precipitation_noise, Vector3(7, 7, 0))
 	display_dict(precipitation_averages, Vector3(7, 0, 0))
+	
+	# Debug height to land/ocean mask
 	display_noise(height_noise, Vector3(14, 7, 0))
 	display_dict(land_ocean_mask, Vector3(14, 0, 0))
-	display_voronoi(Vector3(21, 7, 0))
 
-	var combined_height_image = create_combined_height_image(land_ocean_mask)
-	var normal_map_texture = ImageTexture.create_from_image(combined_height_image)
+	display_noise(smooth_height_noise, Vector3(21, 7, 0))
+	display_image(combined_height_image, Vector3(21, 0, 0))
 
+	# display_voronoi(Vector3(28, 7, 0))
 	var biome_map_image = create_biome_map_image(
 		land_ocean_mask,
 		temperature_averages,
-		precipitation_averages
+		precipitation_averages,
+		true
 	)
-	_threaded_biome_texture = ImageTexture.create_from_image(biome_map_image)
-	_threaded_normal_map_texture = normal_map_texture
-	var biome_texture = ImageTexture.create_from_image(biome_map_image)
-	_threaded_biome_texture = biome_texture
-	
-	display_biome()
-
+	# TODO: use this
+	print(biome_map_image)
 
 func create_combined_height_image(land_ocean_mask: Dictionary) -> Image:
 	var image = Image.create(SIZE, SIZE, false, Image.FORMAT_RF)
@@ -107,7 +112,7 @@ func create_combined_height_image(land_ocean_mask: Dictionary) -> Image:
 			image.set_pixel(x, y, Color(combined_value, combined_value, combined_value))
 	
 	# Normalize to 0-1 range
-	image.convert(Image.FORMAT_RF)
+	image.convert(Image.FORMAT_RGBA8)
 	for x in SIZE:
 		for y in SIZE:
 			var value = image.get_pixel(x, y).r
@@ -116,41 +121,94 @@ func create_combined_height_image(land_ocean_mask: Dictionary) -> Image:
 
 	return image
 
-func create_biome_map_image(land_ocean_mask, temperature_averages, precipitation_averages) -> Image:
+func create_biome_map_image(land_ocean_mask, temperature_averages, precipitation_averages, debug=false) -> Image:
 	var image = Image.create(SIZE, SIZE, false, Image.FORMAT_RGBA8)
-	for x in SIZE:
-		for y in SIZE:
-			var pos = Vector2(x, y)
-			var is_land = land_ocean_mask[pos]
-			if is_land:
-				var temp = temperature_averages[pos]
-				var precip = precipitation_averages[pos]
-				
-				# Quantize to 0-255
-				var quant_temp = int((temp + 1.0) * 127.5)
-				quant_temp = clamp(quant_temp, 0, 255)
-				var quant_precip = int((precip + 1.0) * 127.5)
-				quant_precip = clamp(quant_precip, 0, 255)
-				quant_precip = 255 - quant_precip  # Invert precipitation axis
-				
-				var biome_index = biome_indices[quant_temp][quant_precip]
-				var color = BIOME_COLORS[biome_index]
 
-				# TODO: properly adjust height factor based on biome
-				var height_value = height_noise.get_noise_2d(x, y)
-				var height_factor = clamp((height_value + 1.0) * 0.5, 0.2, 1.0)
-				color.r *= height_factor
-				color.g *= height_factor
-				color.b *= height_factor
-				image.set_pixel(x, y, color)
-			else: # Ocean
-				var ocean_color = Color(0.0, 0.0, 1)
-				var smooth_value = smooth_height_noise.get_noise_2d(x, y)
-				var height_factor = clamp((smooth_value + 1.0) * 0.5, 0.2, 1.0)
-				ocean_color.r *= height_factor
-				ocean_color.g *= height_factor
-				ocean_color.b *= height_factor
-				image.set_pixel(x, y, ocean_color)
+	# TODO: remove temporary debug code
+	if debug:
+		for x in SIZE:
+			for y in SIZE:
+				var pos = Vector2(x, y)
+				var is_land = land_ocean_mask[pos]
+				if is_land:
+					var temp = temperature_averages[pos]
+					var precip = precipitation_averages[pos]
+					
+					# Quantize to 0-255
+					var quant_temp = int((temp + 1.0) * 127.5)
+					quant_temp = clamp(quant_temp, 0, 255)
+					var quant_precip = int((precip + 1.0) * 127.5)
+					quant_precip = clamp(quant_precip, 0, 255)
+					quant_precip = 255 - quant_precip  # Invert precipitation axis
+					
+					var biome_index = biome_indices[quant_temp][quant_precip]
+					var color = BIOME_COLORS[biome_index]
+
+					image.set_pixel(x, y, color)
+				else: # is_ocean
+					var ocean_color = Color(0.0, 0.0, 1)
+					image.set_pixel(x, y, ocean_color)
+
+		display_image(image, Vector3(28, 7, 0))
+
+		for x in SIZE:
+			for y in SIZE:
+				var pos = Vector2(x, y)
+				var is_land = land_ocean_mask[pos]
+				if is_land:
+					var color = image.get_pixel(x, y)
+
+					# TODO: properly adjust height factor based on biome
+					var height_value = height_noise.get_noise_2d(x, y)
+					var height_factor = clamp((height_value + 1.0) * 0.5, 0.2, 1.0)
+					color.r *= height_factor
+					color.g *= height_factor
+					color.b *= height_factor
+					image.set_pixel(x, y, color)
+				else: # is_ocean
+					var ocean_color = Color(0.0, 0.0, 1)
+					var smooth_value = smooth_height_noise.get_noise_2d(x, y)
+					var height_factor = clamp((smooth_value + 1.0) * 0.5, 0.2, 1.0)
+					ocean_color.r *= height_factor
+					ocean_color.g *= height_factor
+					ocean_color.b *= height_factor
+					image.set_pixel(x, y, ocean_color)
+
+		display_image(image, Vector3(28, 0, 0))
+	else:
+		for x in SIZE:
+			for y in SIZE:
+				var pos = Vector2(x, y)
+				var is_land = land_ocean_mask[pos]
+				if is_land:
+					var temp = temperature_averages[pos]
+					var precip = precipitation_averages[pos]
+					
+					# Quantize to 0-255
+					var quant_temp = int((temp + 1.0) * 127.5)
+					quant_temp = clamp(quant_temp, 0, 255)
+					var quant_precip = int((precip + 1.0) * 127.5)
+					quant_precip = clamp(quant_precip, 0, 255)
+					quant_precip = 255 - quant_precip  # Invert precipitation axis
+					
+					var biome_index = biome_indices[quant_temp][quant_precip]
+					var color = BIOME_COLORS[biome_index]
+
+					# TODO: properly adjust height factor based on biome
+					var height_value = height_noise.get_noise_2d(x, y)
+					var height_factor = clamp((height_value + 1.0) * 0.5, 0.2, 1.0)
+					color.r *= height_factor
+					color.g *= height_factor
+					color.b *= height_factor
+					image.set_pixel(x, y, color)
+				else: # is_ocean
+					var ocean_color = Color(0.0, 0.0, 1)
+					var smooth_value = smooth_height_noise.get_noise_2d(x, y)
+					var height_factor = clamp((smooth_value + 1.0) * 0.5, 0.2, 1.0)
+					ocean_color.r *= height_factor
+					ocean_color.g *= height_factor
+					ocean_color.b *= height_factor
+					image.set_pixel(x, y, ocean_color)
 	return image
 
 func get_biome_index(color: Color) -> int:
@@ -225,6 +283,18 @@ func display_dict(dict, offset:Vector3):
 	sprite.position += offset
 	call_deferred("add_child", sprite)
 
+func display_image(image, offset:Vector3):
+	var texture
+	if image is ImageTexture:
+		texture = image
+	else:
+		texture = ImageTexture.create_from_image(image)
+	var sprite = Sprite3D.new()
+	sprite.texture = texture
+	sprite.pixel_size = 0.006
+	sprite.position += offset
+	call_deferred("add_child", sprite)
+
 func dict_to_image(data):
 	var image = Image.create(SIZE, SIZE, false, Image.FORMAT_RGBA8)
 	for x in range(SIZE):
@@ -275,18 +345,4 @@ func display_land_ocean(mask, offset: Vector3):
 	sprite.texture = texture
 	sprite.pixel_size = 0.006
 	sprite.position += offset
-	call_deferred("add_child", sprite)
-
-func display_biome():
-	var sprite = Sprite3D.new()
-	sprite.texture = _threaded_biome_texture
-	sprite.pixel_size = 0.006
-	sprite.position = Vector3(21, 0, 0)
-
-	var material = StandardMaterial3D.new()
-	material.albedo_texture = _threaded_biome_texture
-	material.normal_texture = _threaded_normal_map_texture
-	material.texture_filter = BaseMaterial3D.TEXTURE_FILTER_NEAREST
-	sprite.material_override = material
-
 	call_deferred("add_child", sprite)
