6f04f9d0510b1e811382d520c8f10095f0e6e2db
lemontine <computereality@gmail.com>
2025-02-04 22:26:11 -0800

    setup item_drop collisions with player

--- godot/item_collect.gd
+++ godot/item_collect.gd
@@ -1,14 +1,11 @@
 extends RigidBody3D
 
 
-func _on_area_3d_body_entered(body: Node) -> void:
-	# body detected should be the CharacterBody3D. It should also have the metadata of Name with the value "agent"
-	var meta_name = body.get_meta("Name")
-	if(meta_name == "agent"):
-		print("It's touching me!!!")
-		if(body.has_node("InventoryManager")):
-			var body_node = body.get_node("InventoryManager")
-			#Your IDE might mark body_node with a red squiggle but the code works fine
-			body_node.PrintInventory()
-
-	
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	if body.has_meta("Name"):
+		if body.get_meta("Name") == "agent":
+			print("It's touching me!!!")
+			# TODO: Add item to inventory
+			# if(body.has_node("InventoryManager")):
+			# 	var body_node = body.get_node("InventoryManager")
+			# 	body_node.PrintInventory()
