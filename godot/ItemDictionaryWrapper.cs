6e082702166a3dd031769d6c50225f8b1da5fcca
Keshfer <Keshfur@gmail.com>
2025-02-07 21:21:20 -0800

    Created ItemDictionaryWrapper

---
+++ godot/ItemDictionaryWrapper.cs
@@ -0,0 +1,18 @@
+using Godot;
+using System;
+
+public partial class ItemDictionaryWrapper : Resource
+{
+    private static ItemDictionary instance = null;
+
+    // public static ItemDictionary GetInstance()
+    // {
+    //     if(instance == null)
+    //     {
+    //         instance = new ItemDictionary();
+    //     }
+    //     return instance;
+            
+    // }
+
+}
