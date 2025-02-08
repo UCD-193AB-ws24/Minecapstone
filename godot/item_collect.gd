e1650d29be6a47999028fd679e491fa433ade4b7
Keshfer <Keshfur@gmail.com>
2025-02-07 21:20:46 -0800

    Completed item_collect functionality

--- godot/item_collect.gd
+++ godot/item_collect.gd
@@ -4,8 +4,14 @@ extends RigidBody3D
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body.has_meta("Name"):
 		if body.get_meta("Name") == "agent":
-			print("It's touching me!!!")
-			# TODO: Add item to inventory
-			# if(body.has_node("InventoryManager")):
-			# 	var body_node = body.get_node("InventoryManager")
-			# 	body_node.PrintInventory()
+			#print("It's touching me!!!")
+			#print("Item name is " + self.get_meta("ItemName"))
+			if(body.has_node("InventoryManager")):
+				var item_name = get_meta("ItemName");
+				var inventory_node = body.get_node("InventoryManager")
+				var itemdict_instance = load("res://ItemDictionary.cs").new()
+				print(itemdict_instance.Get(item_name).PrintItem())
+				var block = itemdict_instance.Get(item_name)
+				inventory_node.AddItem(block, 1)
+				queue_free()
+				inventory_node.PrintInventory()
