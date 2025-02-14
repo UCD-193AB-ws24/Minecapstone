9f2b536ca36b8747c81d78775c1f176315b1bdc3
lemontine <computereality@gmail.com>
2025-02-14 01:54:43 -0800

    sky shader dev

--- godot/prefabs/player.gd
+++ godot/prefabs/player.gd
@@ -71,6 +71,7 @@ func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	global_position = spawn_point.global_position
 	inventory_manager.AddItem(itemdict_instance.Get("Stone"), 64)
+	inventory_manager.AddItem(itemdict_instance.Get("Sand"), 64)
 	inventory_manager.AddItem(itemdict_instance.Get("Wood Pickaxe"), 1)
 
 
@@ -138,7 +139,7 @@ func _physics_process(_delta):
 	_update_fov(_delta)
 	_update_health_hunger_thirst(_delta)
 
-	if global_position.y < 15:
+	if global_position.y < 0:
 		_on_out_of_bounds()
 
 
