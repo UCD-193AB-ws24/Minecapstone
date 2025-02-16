3f45308f740032990cf029985342e5a380d04d56
Keshfer <Keshfur@gmail.com>
2025-02-15 22:31:23 -0800

    call_move_to function works. agent.tscn is a duplicate of player.tscn
    You can delete the _ready function under #test.

--- godot/prefabs/LLM_methods.gd
+++ godot/prefabs/LLM_methods.gd
@@ -1,1 +1,11 @@
-class_name LLM_Methods extends Node
+class_name LLM_Methods extends NPC
+
+
+#test
+func _ready() -> void:
+	super()
+	set_movement_target(Vector3(1,0,10))
+
+
+func call_move_to(dest: Vector3):
+	set_movement_target(dest)
