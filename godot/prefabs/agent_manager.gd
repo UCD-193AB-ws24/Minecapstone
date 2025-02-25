fbc87259014ded1c44ee18fb56021aee79eb1f66
Keshfer <Keshfur@gmail.com>
2025-02-24 16:27:28 -0800

    Completed agent_manager and made it a prefab

--- godot/prefabs/agent_manager.gd
+++ godot/prefabs/agent_manager.gd
@@ -1,22 +1,25 @@
 extends Node
 
 var agent_dict = {}
-var world = self.get_parent()
+var world
 var agent_scene = preload("res://prefabs/agent.tscn")
 
 func _ready() -> void:
-    #find all agents placed through Godot editor
-    for node in world.get_children():
-        if node.has_meta("Name"):
-            continue
-        var node_meta_name = node.get_meta("Name")
-        if node_meta_name == "agent":
-            agent_dict[node.name] = node
+	world = self.get_parent()
+	#find all agents placed through Godot editor
+	for node in world.get_children():
+		if not (node.has_meta("Name")):
+			continue
+		var node_meta_name = node.get_meta("Name")
+		if node_meta_name == "agent":
+			agent_dict[node.name] = node
 
 func spawn_agent(agent_name:String):
-    
-    var agent_instance = agent_scene.instantiate()
-    agent_instance.name = agent_name
-    agent_dict[agent_name] = agent_instance
-    world.add_child(agent_instance)
+	
+	var agent_instance = agent_scene.instantiate()
+	agent_instance.name = agent_name
+	agent_dict[agent_name] = agent_instance
+	world.add_child(agent_instance)
 
+func get_agent(agent_name:String):
+	return agent_dict[agent_name]
