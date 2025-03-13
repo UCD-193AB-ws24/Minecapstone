20f597596e4b7086a6f2bc5302833da71d41cd8d
Darroll <computereality@gmail.com>
2025-03-12 17:03:09 -0700

    Revert InventoryManager

--- godot/prefabs/player.gd
+++ godot/prefabs/player.gd
@@ -338,6 +338,7 @@ func _break_block():
 
 func _handle_attacking():
 	if raycast.is_colliding() and raycast.get_collider() is Player:
+		print("Attack!")
 		var target = raycast.get_collider()
 		if Input.is_action_just_pressed("mouse1"):
 			target.damage(10)
