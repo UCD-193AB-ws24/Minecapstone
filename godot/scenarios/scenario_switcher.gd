c3e1af62a0a5ef98131bd69475a8eeffd2bcb423
Darroll <computereality@gmail.com>
2025-04-25 16:55:08 -0700

    implemented scenario switching

--- godot/scenarios/scenario_switcher.gd
+++ godot/scenarios/scenario_switcher.gd
@@ -2,3 +2,7 @@ class_name ScenarioSwitcher
 extends Node
 
 @export var scene_list: Array[PackedScene] = []
+
+func _ready() -> void:
+	print(scene_list[0].can_instantiate())
+	get_tree().change_scene_to_packed(scene_list[0])
