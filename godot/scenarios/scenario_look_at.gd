b13ac354243b4f16e15cb88c9bee529ce6750a45
JonLagasca <jonrlagasca@gmail.com>
2025-04-30 15:45:27 -0700

    Attempted debugging the scenario after reset

--- godot/scenarios/scenario_look_at.gd
+++ godot/scenarios/scenario_look_at.gd
@@ -1,27 +1,51 @@
 # LOOK AT TARGET SCENARIO
 extends ScenarioManager
 
-
-
+var agent
+var raycast
 
 func _ready() -> void:
 	super()
 	reload()
 
+func _find_agent():
+	agent = get_parent().find_child("Agent")
+	if not agent:
+		print("Agent not found")
+		return
+	else:
+		raycast = agent.get_node("Head").get_node("Camera3D").get_node("RayCast3D")
+		print("Agent: ", agent)
+		print("Raycast: ", raycast)
+
+func reset():
+	super()
+
+	print("FINDING THE AGENT NOW")
+	_find_agent()
+
 func reload():
+	_find_agent()
+
 	success_count = 0
 	failure_count = 0
 
-
-# currently broken, physics process does not continue after reset
+# currently broken, still trying to figure out how to get raycast after a reset
 func _physics_process(_delta):
-	var raycast = get_parent().find_child("Agent").get_node("Head").get_node("Camera3D").get_node("RayCast3D")
+	# raycast = get_parent().get_node("Agent").get_node("Head").get_node("Camera3D").get_node("RayCast3D")
+	# print("Finding raycast")
+	if raycast == null:
+		print("Raycast not found")
+		_find_agent()
+	
+	# print("Raycast found")
+
 	if raycast.is_colliding():
 		print("Colliding")
 		var collider = raycast.get_collider()
 		if collider.name == "NPCZombie":
 			track_success()
-		elif collider.name == "NPCZombie":
+		elif collider.name == "Animal":
 			track_failure()
 		if current_iteration <= MAX_ITERATIONS:
 			reset()
