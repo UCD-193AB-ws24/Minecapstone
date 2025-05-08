b446f3cf9819908c7fc69e4eedef43b1768d04ef
Darroll <computereality@gmail.com>
2025-05-07 17:52:08 -0700

    Remove unnecessary variable already_processed from scenario_say

--- godot/scenarios/scenario_say.gd
+++ godot/scenarios/scenario_say.gd
@@ -1,7 +1,6 @@
 extends ScenarioManager
 
 var test_message = "Hello, I am excited to play Minecapstone"
-var already_processed = false
 
 func _ready() -> void:
 	super()
@@ -12,7 +11,6 @@ func _ready() -> void:
 func _on_message_received(msg: String, _from_id: int, _to_id: int) -> void:
 	# Check for our test message
 	if msg == test_message:
-		already_processed = true
 		track_success()
 
 		if current_iteration < MAX_ITERATIONS:
