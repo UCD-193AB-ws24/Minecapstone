8964a7afe7fbac871d3f24d809d7ffa4a77b6116
lemontine <computereality@gmail.com>
2025-05-15 00:11:11 -0700

    basic environment setup for puzzle scene

---
+++ godot/benchmarking/scenarios/scenario_complex_puzzle.gd
@@ -0,0 +1,24 @@
+# COMPLEX PUZZLE SCENARIO
+extends ScenarioManager
+
+
+func _ready() -> void:
+	super()
+	reload()
+
+
+func reload():
+	var red_platform = get_parent().find_child("RedPlatform").get_node("Area3D")
+	var blue_platform = get_parent().find_child("BluePlatform").get_node("Area3D")
+	
+	success_count = 0
+	failure_count = 0
+	red_platform.connect("body_entered", _on_touch_platform.bind("RedPlatform"))
+	blue_platform.connect("body_entered", _on_touch_platform.bind("BluePlatform"))
+
+
+func _on_touch_platform(_body: Node3D, platform_name: String) -> void:
+	if platform_name == "RedPlatform":
+		track_success()
+	else:
+		track_failure()
